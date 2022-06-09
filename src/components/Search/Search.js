a9df547e77d3ed9f1dfdb8170c30e9dab4d9017a
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-06-10 00:42:17 +0530

    feat: add search component

---
+++ src/components/Search/Search.js
@@ -0,0 +1,15 @@
+import "./Search.css";
+
+const Search = () => {
+  return (
+    <>
+      <input
+        className="book-search"
+        type="text"
+        placeholder="Search Books Name Here...."
+      />
+    </>
+  );
+};
+
+export { Search };
