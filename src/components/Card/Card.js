bd4b1e8d664bd25d908c76f575f4c14118fc93a7
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-06-10 17:16:21 +0530

    feat: add book card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,38 @@
+import "./Card.css";
+
+const Card = ({ book }) => {
+  return (
+    <div className="book-card-wrapper">
+      <div className="book-card-image-wrapper">
+        <img
+          className="book-thumbnail"
+          src={book.imageLinks.thumbnail}
+          alt={book.volumeInfo.title}
+        />
+      </div>
+      <div className="book-card-header">
+        <h2 className="book-card-heading">{book.volumeInfo.title}</h2>
+        <small className="book-card-sub-heading">
+          {book.volumeInfo.publisher}
+        </small>
+      </div>
+      <div className="book-card-body">
+        <div className="book-card-details-wrapper mt-2">
+          <div className="book-published-date">
+            {book.volumeInfo.publishedDate}
+          </div>
+          <div className="book-rating">
+            Rating {book.volumeInfo.averageRating}
+          </div>
+        </div>
+      </div>
+      <div className="book-card-footer">
+        <button type="button" className="book-view-btn">
+          View Book
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export { Card };
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { Search } from "./Search/Search";
+export { Card } from "./Card/Card";
