970ff8b1c59c83c3bf5c6512439f68e0fd41f84d
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-06-14 01:44:54 +0530

    feat: add 404 page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import { Homepage, SingleBook } from "./pages";
+import { Homepage, NotFoundPage, SingleBook } from "./pages";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/book/:bookId" element={<SingleBook />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
---
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,14 @@
+const NotFoundPage = () => {
+  return (
+    <video
+      loading="lazy"
+      muted="muted"
+      src="/assets/404-video.mp4"
+      type="video/mp4"
+      autoplay="autoplay"
+      loop="loop"
+    ></video>
+  );
+};
+
+export { NotFoundPage };
