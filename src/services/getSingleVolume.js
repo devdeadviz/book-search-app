b4859f7c89fdd951b48b37b1b6288d058dbd7e2d
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-06-14 19:16:26 +0530

    refactor: add env file and shift api to env file

--- src/services/getSingleVolume.js
+++ src/services/getSingleVolume.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const getSingleVolume = async (bookId) => {
   try {
     const { data } = await axios.get(
-      `https://www.googleapis.com/books/v1/volumes/${bookId}?key=AIzaSyCB9PJwAtu0cslP4CTeVqh3r2vTqgVwcco`
+      `https://www.googleapis.com/books/v1/volumes/${bookId}?key=${process.env.REACT_APP_API_KEY}`
     );
     return data;
   } catch (error) {
